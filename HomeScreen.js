74b24d6ad633716551a891333af945917d4e31fd
jcarte <jcarter1987@gmail.com>
2024-01-25 19:51:47 +0000

    Try to fix issue with bottom sheet now appearing on 2nd load of actual build by reverting home screen to function as advised rather than class

--- HomeScreen.js
+++ HomeScreen.js
@@ -8,36 +8,25 @@ import { MessageComponent } from './MessageComponent.js';
 
 import BottomSheet from '@gorhom/bottom-sheet';
 
-//route, navigation }
-class HomeScreen extends React.Component  {
+export function HomeScreen() {
 
-  constructor(props)
-  {
-    console.log("Home: run constructor")
-
-    super(props)
-    this.state = {
+  const[state, setState] = useState(
+    {
       allowScanning: true,
       results: {
         status: "init",
         product: null
       },
       isResultsExpanded: false,
-    }
-    this.ph = new ProductHandler()
+    })
 
-    this.handleBarCode = this.handleBarCode.bind(this)
-    this.getMessageText = this.getMessageText.bind(this)
-    this.handleSheetChanges = this.handleSheetChanges.bind(this)
-    
-
-
-    //For bottom sheet
-    //this.sheetRef = useRef(null);
-    //this.snapPoints = useMemo(() => {return results.status==='ok'? [240,"90%"]: [240]});
-  }
+  const ph = new ProductHandler()
 
   
+    //For bottom sheet
+  const sheetRef = useRef(null);
+  let snapPoints = useMemo(() => {return state?.results?.status==='ok'? [240,"90%"]: [240]});
+
 
   // useMemo(() => {
   //   this.snapPoints = results.status==='ok'? [240,"90%"]: [240]
@@ -54,40 +43,52 @@ class HomeScreen extends React.Component  {
   handleBarCode = async (bc) => {
     console.log('Home: Handle barcode:', bc)
 
-    console.log('Home: handleBarCode: allow scanning?:', this.state.allowScanning)
-    console.log('Home: handleBarCode: is results expanded?:', this.state.isResultsExpanded)
+    console.log('Home: handleBarCode: allow scanning?:', state.allowScanning)
+    console.log('Home: handleBarCode: is results expanded?:', state.isResultsExpanded)
 
     
     
 
     console.log("Home: stop allowing scanning")
     //setAllowScanning(false)
-    this.setState({allowScanning: false})
+    setState({
+      allowScanning: false,
+      results: state.results,
+      isResultsExpanded: state.isResultsExpanded,
+    })
 
-    if(this.state.isResultsExpanded)//if results are up then abandon
+    if(state.isResultsExpanded)//if results are up then abandon
     {
       console.log('Home: handleBarCode: abandoning as results are up?')
     }
-    else if(bc === this.state.results.product?.barcode)
+    else if(bc === state.results.product?.barcode)
     {
       console.log("Home: same barcode detected, skipping fetch")
     }
     else
     {
       console.log("Home: barcode fetch:",bc)
-      const p = await this.ph.FetchProduct(bc)
+      const p = await ph.FetchProduct(bc)
       //setResults(p)
-      this.setState({results: p})
+      setState({
+        allowScanning: state.allowScanning,
+        results: p,
+        isResultsExpanded: state.isResultsExpanded,
+      })
     }
     
     console.log("Home: allow scanning in 3 secs")
     // setTimeout(() => { setAllowScanning(true); }, 3000)
-    setTimeout(() => { this.setState({allowScanning: true}); }, 3000)
+    setTimeout(() => { setState({
+      allowScanning: true,
+      results: state.results,
+      isResultsExpanded: state.isResultsExpanded,
+    }); }, 3000)
   }
 
   getMessageText = () =>
   {
-    switch (this.state.results.status) {
+    switch (state.results.status) {
       case "init":
         return "Scan a product barcode to start"
       case "error":
@@ -104,7 +105,6 @@ class HomeScreen extends React.Component  {
 
 
 
-  //DOESN'T WORK
   // callbacks
   handleSheetChanges = (index) => {
     console.log('Home: handleSheetChanges', index)
@@ -112,40 +112,46 @@ class HomeScreen extends React.Component  {
     //if maximising results stop scanning
     if(index === 1)
     {
-      this.setState({isResultsExpanded: true})
+      setState({
+        allowScanning: state.allowScanning,
+        results: state.results,
+        isResultsExpanded: true,
+      })
     }
     else
     {
-      this.setState({isResultsExpanded: false})
+      setState({
+        allowScanning: state.allowScanning,
+        results: state.results,
+        isResultsExpanded: false,
+      })
     }
       
   }
 
-  render() {
+  
     return (
       <View style={StyleSheet.absoluteFillObject}>
         <BottomSheet
-          ref={null}
+          ref={sheetRef}
           index={0}
-          onChange={this.handleSheetChanges}
-          snapPoints={this.state.results.status==='ok'? [240,"90%"]: [240]}
-          handleIndicatorStyle={!this.state.results.product && {opacity: 0, height: 0}}//only show resize handle if can resize
+          onChange={handleSheetChanges}
+          snapPoints={snapPoints}
+          handleIndicatorStyle={!state.results.product && {opacity: 0, height: 0}}//only show resize handle if can resize
           backdropComponent={()=>
             <View style={StyleSheet.absoluteFillObject}>
-              <BarcodeScannerComponent onBarCodeScanned ={this.state.allowScanning? this.handleBarCode : undefined} allowScanning ={this.state.allowScanning}/>
+              <BarcodeScannerComponent onBarCodeScanned ={state.allowScanning? handleBarCode : undefined} allowScanning ={state.allowScanning}/>
             </View>
           }
         >
-          {this.state.results.status !== 'ok' && <MessageComponent messageText={this.getMessageText()}  style={StyleSheet.absoluteFillObject}/>}
-          {this.state.results.product && <FullResultsComponent product={this.state.results.product}/>}
+          {state.results.status !== 'ok' && <MessageComponent messageText={getMessageText()}  style={StyleSheet.absoluteFillObject}/>}
+          {state.results.product && <FullResultsComponent product={state.results.product}/>}
         </BottomSheet>
       </View>
     )
-  }
   
 
 }
-export default HomeScreen
 
 /*
 
