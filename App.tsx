648aa9a045a830f1a107744029f5012e11cad063
jcarte <jcarter1987@gmail.com>
2024-03-14 17:50:31 +0000

    Add sentry

--- App.tsx
+++ App.tsx
@@ -6,20 +6,28 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { HomeScreen } from './src/screens/HomeScreen';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 
+import * as Sentry from '@sentry/react-native';
+
 export const Stack = createNativeStackNavigator();
 
-export default function App() {
+Sentry.init({
+  dsn: __DEV__? undefined : 'https://120475c6ff30d996e1167a7e38c5ec91@o4506823877984256.ingest.us.sentry.io/4506823900200960',
+  debug: false, // If `true`, Sentry will try to print out useful debugging information if something goes wrong with sending the event. Set it to `false` in production
+});
+
+function App() {
 
   return (
-      <SafeAreaProvider>
-    <GestureHandlerRootView style={{ flex: 1 }}>
-        <NavigationContainer>
-          <Stack.Navigator initialRouteName="Home" screenOptions={{headerShown: false}}>
-            <Stack.Screen name="Home" component={HomeScreen} initialParams={{}}/>
-          </Stack.Navigator>
-        </NavigationContainer>
-    </GestureHandlerRootView>
-      </SafeAreaProvider>
+    <SafeAreaProvider>
+      <GestureHandlerRootView style={{ flex: 1 }}>
+          <NavigationContainer>
+            <Stack.Navigator initialRouteName="Home" screenOptions={{headerShown: false}}>
+              <Stack.Screen name="Home" component={HomeScreen} initialParams={{}}/>
+            </Stack.Navigator>
+          </NavigationContainer>
+      </GestureHandlerRootView>
+    </SafeAreaProvider>
   );
 
 }
+export default Sentry.wrap(App);
